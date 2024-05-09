c27b54017e70dedc2f0373c011b13573b0b0bfc0
ssshekhu53 <ssshekhu53@gmail.com>
2024-05-09 18:27:11 +0530

    Update metrics methods signature and update Client struct
    Update signature of methods
    Add QueryRow method

--- cassandra.go
+++ cassandra.go
@@ -1,6 +1,9 @@
 package cassandra
 
 import (
+	"encoding/json"
+	"errors"
+	"strconv"
 	"strings"
 
 	"github.com/gocql/gocql"
@@ -14,9 +17,11 @@ type Client struct {
 }
 
 func New(conf Config, logger Logger, metrics Metrics) *Client {
-	hosts := strings.Split(conf.Get("CASSANDRA_HOSTS"), ",")
+	hosts := strings.Split(conf.Get("CASS_DB_HOST"), ",")
 	cluster := gocql.NewCluster(hosts...)
-	cluster.Keyspace = conf.Get("CASSANDRA_KEYSPACE")
+	cluster.Keyspace = conf.Get("CASS_DB_KEYSPACE")
+	cluster.Port, _ = strconv.Atoi(conf.Get("CASS_DB_PORT"))
+	cluster.Authenticator = gocql.PasswordAuthenticator{Username: conf.Get("CASS_DB_USER"), Password: conf.Get("CASS_DB_PASS")}
 
 	session, err := cluster.CreateSession()
 	if err != nil {
@@ -28,14 +33,54 @@ func New(conf Config, logger Logger, metrics Metrics) *Client {
 	return &Client{Session: session, logger: logger, metrics: metrics}
 }
 
-func (c *Client) Query(stmt string, values ...interface{}) *gocql.Query {
-	return c.Session.Query(stmt, values...)
+func (c *Client) Query(result interface{}, stmt string, values ...interface{}) error {
+	var err error
+
+	resultMap, err := c.Session.Query(stmt, values...).Iter().SliceMap()
+	if err != nil {
+		return err
+	}
+
+	err = c.parseData(resultMap, result)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
-func (c *Client) Iter(stmt string, values ...interface{}) *gocql.Iter {
-	return c.Session.Query(stmt, values...).Iter()
+func (c *Client) QueryRow(result interface{}, stmt string, values ...interface{}) error {
+	var err error
+
+	resultMap := make(map[string]interface{})
+
+	ok := c.Session.Query(stmt, values...).Iter().MapScan(resultMap)
+	if !ok {
+		return errors.New("unable to fetch result")
+	}
+
+	err = c.parseData(resultMap, result)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
 func (c *Client) Exec(stmt string, values ...interface{}) error {
 	return c.Session.Query(stmt, values...).Exec()
+}
+
+func (c *Client) parseData(resultMap interface{}, result interface{}) error {
+	data, err := json.Marshal(resultMap)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- metrics.go
+++ metrics.go
@@ -11,5 +11,4 @@ type Metrics interface {
 	IncrementCounter(ctx context.Context, name string, labels ...string)
 	DeltaUpDownCounter(ctx context.Context, name string, value float64, labels ...string)
 	RecordHistogram(ctx context.Context, name string, value float64, labels ...string)
-	SetGauge(name string, value float64)
 }
